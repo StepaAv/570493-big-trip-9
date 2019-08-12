07b65577930772b3da4f2dae3f328825042d85d0
StepaAv <353833@gmail.com>
2019-08-12 09:42:44 +0300

    insertadjasenhtml position fix towards to H2

--- src/main.js
+++ src/main.js
@@ -4,20 +4,18 @@ import {createBlockTripFilters} from './components/block-trip-filters.js';
 import {createBlockTripEventsSort} from './components/block-trip-events-sort.js';
 import {createBlockTripEventsContent} from './components/block-trip-events-content.js';
 
-// document.getElementByClassName('.trip-info__cost').style.display = none;
-
-
 const mainBlockMainTrip = document.querySelector(`.trip-main__trip-info`);
-const mainBlockTripControl = document.querySelector(`.trip-main__trip-controls`);
 const mainBlockTripEvents = document.querySelector(`.trip-events`);
+const mainBlockTripControlTabs = document.querySelector(`.trip-main__trip-controls > h2:first-child`);
+const mainBlockTripControlFilters = document.querySelector(`.trip-main__trip-controls > h2:last-child`);
 
 const renderBlocks = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 renderBlocks(mainBlockMainTrip, createBlockMainTripInfo(), `beforeend`);
-renderBlocks(mainBlockTripControl, createBlockTripControlTabs(), `afterbegin`);
-renderBlocks(mainBlockTripControl, createBlockTripFilters(), `beforeend`);
+renderBlocks(mainBlockTripControlTabs, createBlockTripControlTabs(), `afterend`);
+renderBlocks(mainBlockTripControlFilters, createBlockTripFilters(), `afterend`);
 renderBlocks(mainBlockTripEvents, createBlockTripEventsSort(), `beforeend`);
 renderBlocks(mainBlockTripEvents, createBlockTripEventsContent(), `beforeend`);
 
