0bbb74021653b678fb17f074a099a7cc32967fca
StepaAv <353833@gmail.com>
2019-10-05 22:16:40 +0300

    transfer to PC

---
+++ src/components/block-additional-offers.js
@@ -0,0 +1,8 @@
+export const createBlockAdditionalOffer = () => {
+    return `
+    <li class="event__offer">
+                        <span class="event__offer-title"></span>
+                        +
+                        €&nbsp;<span class="event__offer-price"></span>
+                       </li>`
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,36 @@
 import {
-  createBlockMainTripInfo
+    createBlockMainTripInfo
 } from './components/block-main-trip-info.js';
 import {
-  createBlockTripControlTabs
+    createBlockTripControlTabs
 } from './components/block-trip-control-tabs.js';
 import {
-  createBlockTripFilters
+    createBlockTripFilters
 } from './components/block-trip-filters.js';
 import {
-  createTripEventsForm
+    createTripEventsForm
 } from './components/block-trip-events-form.js';
 import {
-  createAllMokData
+    createAllMokData
 } from './components/data.js';
 import {
-  addFirstTripEvent
+    addFirstTripEvent
 } from './components/block-add-trip-event.js';
 import {
-  createBlockTripEventsSort
+    createBlockTripEventsSort
 } from './components/block-trip-events-sort.js';
 import {
-  createBlockTripDays
+    createBlockTripDays
 } from './components/block-trip-days.js';
 import {
-  createBlockTripDay
+    createBlockTripDay
 } from './components/block-trip-day.js';
 import {
-  createEditTripEvent
+    createEditTripEvent
 } from './components/block-edit-trip-event.js';
+import {
+    createBlockAdditionalOffer
+} from './components/block-additional-offers.js';
 
 
 const mainBlockMainTrip = document.querySelector(`.trip-main__trip-info`);
@@ -39,7 +42,7 @@ const mainEventAddBtn = document.querySelector(`.trip-main__event-add-btn`);
 
 
 const renderBlocks = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+    container.insertAdjacentHTML(place, template);
 };
 
 renderBlocks(mainBlockMainTrip, createBlockMainTripInfo(), `beforeend`);
@@ -49,8 +52,8 @@ renderBlocks(mainBlockTripEventsForm, createTripEventsForm(), `beforeend`);
 
 
 const generateEveryMokData = (quantity = 1) => {
-  const routeData = Array(quantity).fill().map(createAllMokData);
-  return routeData;
+    const routeData = Array(quantity).fill().map(createAllMokData);
+    return routeData;
 };
 
 
@@ -60,148 +63,139 @@ let eventNumber = 0;
 let allEventsArray = [];
 
 
-const renderFirstEvent = () => {
-  const mainBlockOneTripEvent = document.querySelector(`.trip-events__item`);
-  temporallyDataArray = generateEveryMokData();
-  eventHeaderData = temporallyDataArray[0];
-  document.onload = renderBlocks(mainBlockOneTripEvent, addFirstTripEvent(eventHeaderData), `beforeend`);
-  document.onload = mainEventAddBtn.disabled = true;
-};
-renderFirstEvent();
+const mainBlockOneTripEvent = document.querySelector(`.trip-events__item`);
 
-
-const saveBtn = document.querySelector(`.event__save-btn`);
-const mainBlockInsertTripDays = document.querySelector(`.trip-events__item`);
-
-
-const pushDataToEventsArrayOnce = () => {
-  // sohraniajem pervyj event, s uslovijem jesli masiv pustoj, jesli net, to sohraniat budet funkcija newEvent
-  if (allEventsArray && allEventsArray.length) {
-    return;
-  } else {
-    allEventsArray.push(eventHeaderData);
-  }
-
-};
 
 const renderTripDayBlock = (number = 0) => {
-  const mainBlockInsertTripDay = document.querySelector(`.trip-events__list`);
-  renderBlocks(mainBlockInsertTripDay, createBlockTripDay(allEventsArray[number]), `afterbegin`);
+    const mainBlockInsertTripDay = document.querySelector(`.trip-events__list`);
+    renderBlocks(mainBlockInsertTripDay, createBlockTripDay(allEventsArray[number]), `afterbegin`);
 };
 
 const renderNewEvent = (number) => {
-  const mainBlockOneTripEvent = document.querySelector(`.trip-events__item`);
-  renderBlocks(mainBlockOneTripEvent, addFirstTripEvent(allEventsArray[number]), `afterbegin`);
+    const mainBlockOneTripEvent = document.querySelector(`.trip-events__item`);
+    renderBlocks(mainBlockOneTripEvent, addFirstTripEvent(allEventsArray[number]), `afterbegin`);
 };
 
 const renderDaysSortBlocks = () => {
-  renderBlocks(mainBlockInsertTripEventsSort, createBlockTripEventsSort(), `afterend`);
-  renderBlocks(mainBlockInsertTripDays, createBlockTripDays(), `afterend`);
+    renderBlocks(mainBlockInsertTripEventsSort, createBlockTripEventsSort(), `afterend`);
+    renderBlocks(mainBlockOneTripEvent, createBlockTripDays(), `afterend`);
 };
 const ifContainsSortBlock = () => {
-  const eventSortBlock = document.querySelector(`.trip-events__trip-sort`);
-  if (document.contains(eventSortBlock)) {
-    return;
-  } else {
-    renderDaysSortBlocks();
-  }
+    const eventSortBlock = document.querySelector(`.trip-events__trip-sort`);
+    if (document.contains(eventSortBlock)) {
+        return;
+    } else {
+        renderDaysSortBlocks();
+    }
 };
 
 const removingEventHeader = () => {
-  const eventHeaderBlock = document.querySelector(`.event__header`);
-  eventHeaderBlock.remove();
+    const eventHeaderBlock = document.querySelector(`.event__header`);
+    eventHeaderBlock.remove();
 };
 
 const newEventBtnDisable = () => {
-  mainEventAddBtn.disabled = true;
+    mainEventAddBtn.disabled = true;
 
 };
 const newEventBtnEnable = () => {
-  mainEventAddBtn.disabled = false;
+    mainEventAddBtn.disabled = false;
 
 };
 let oneEventContainer;
 let arrowDownBtn;
 let oneEvent;
 const savingEventsToArr = () => {
-  oneEventContainer = [...document.querySelectorAll(`.trip-events__item`)];
-  oneEventContainer.shift();
-  // poluchaju masiv knopok so strelkoj vniz
-  arrowDownBtn = [...document.querySelectorAll(`.event__rollup-btn`)];
-  oneEvent = [...document.querySelectorAll(`div .event`)];
-  oneEvent.shift();
+    oneEventContainer = [...document.querySelectorAll(`.trip-events__item`)];
+    oneEventContainer.shift();
+    // poluchaju masiv knopok so strelkoj vniz
+    arrowDownBtn = [...document.querySelectorAll(`.event__rollup-btn`)];
+    oneEvent = [...document.querySelectorAll(`div .event`)];
+    oneEvent.shift();
 };
 
+// const renderOffers = () => {
+//   const renderOffersPlace = document.querySelector('.event__selected-offers');
+//   // for (let offer of allEventsArray) {
+//   //   for (let key of offer.additionalOffers) {
+//   //     if (key.check === true) {
+//   //       console.log('its true');
+//   //       console.log(key);
+//   //     }
+//   //   }
+//   // }
+
+//   for (let i = 0; i < allEventsArray.lenght; i++) {
+//     console.log(allEventsArray[i]);
+//     // for (let k = 0; k < allEventsArray.)
+//   }
+// };
 let formEditBlock;
-
-const makingSaveEvent = () => {
-  pushDataToEventsArrayOnce();
-  removingEventHeader();
-  ifContainsSortBlock();
-  renderTripDayBlock(eventNumber);
-  newEventBtnEnable();
-  savingEventsToArr();
-  getTotalPrice();
-
-  function renderSettingBlock(e) {
-    let counter = arrowDownBtn.indexOf(e.target);
-    const reversedEventsArr = [...allEventsArray];
-    reversedEventsArr.reverse();
-    console.log(formEditBlock);
-    if (document.contains(formEditBlock)) {
-      formEditBlock.remove();
-      oneEvent.map((segment) => segment.style.display = `flex`);
-    }
-    oneEvent[counter].style.display = `none`;
-    renderBlocks(oneEventContainer[counter], createEditTripEvent(reversedEventsArr[counter]), `afterbegin`);
-    formEditBlock = document.querySelector(`.trip-events__item > form.event--edit`);
-    const arrowUpBtn = document.querySelector(`.event--edit > .event__header > .event__rollup-btn`);
-
-    function removeModal() {
-      this.parentNode.parentNode.remove();
-      oneEvent.map((segment) => segment.style.display = `flex`);
-    }
-
-    arrowUpBtn.addEventListener(`click`, removeModal);
-
-  }
-  arrowDownBtn.every((cell) => cell.addEventListener(`click`, renderSettingBlock));
-};
-
-saveBtn.addEventListener(`click`, makingSaveEvent);
+let saveButton;
 
 const makingNewEvent = () => {
-  console.log(formEditBlock);
-  if (document.contains(formEditBlock)) {
-    formEditBlock.remove();
-    oneEvent.map((segment) => segment.style.display = `flex`);
-  }
-  // schetchik po kliku, ukazyvajet na element v masive s mokami
-  eventNumber += 1;
-  // dizablim knopku new event
-  newEventBtnDisable();
-  // sohraniajem mok danyje vo vremenyj massiv
-  temporallyDataArray = generateEveryMokData();
-  // sohraniajem danyje kazhdoj kartochki v masiv
-  allEventsArray.push(temporallyDataArray[0]);
-  // renderim novyh event header s mokami iz allEventsArray
-  renderNewEvent(eventNumber);
-  const saveAnyEvent = document.querySelector(`.event__save-btn`).onclick = makingSaveEvent;
+    if (document.contains(formEditBlock)) {
+        formEditBlock.remove();
+        oneEvent.map((segment) => segment.style.display = `flex`);
+    };
+    newEventBtnDisable();
+    temporallyDataArray = generateEveryMokData();
+    allEventsArray.push(temporallyDataArray[0]);
+    renderNewEvent(eventNumber);
+    saveButton = document.querySelector('.event__save-btn');
 };
+document.onload = makingNewEvent();
 mainEventAddBtn.addEventListener(`click`, makingNewEvent);
 
-const getTotalPrice = () => {
-  const totalSummBlock = document.querySelector(`.trip-info__cost-value`);
-  totalSummBlock.innerHTML = ``;
-  let totalPrice = 0;
-  for (let value of allEventsArray) {
-    totalPrice += value.price;
-    for (let offers of value.additionalOffers) {
-      if (offers.check === true) {
-        totalPrice += offers.price;
-      }
-    }
+const makingSaveEvent = () => {
+    console.log('makingNewEvent clicked');
+    removingEventHeader();
+    ifContainsSortBlock();
+    renderTripDayBlock(eventNumber);
+    newEventBtnEnable();
+    savingEventsToArr();
+    getTotalPrice();
+    eventNumber += 1;
 
-  }
-  renderBlocks(totalSummBlock, totalPrice, `beforeend`);
+    function renderSettingBlock(e) {
+        let counter = arrowDownBtn.indexOf(e.target);
+        const reversedEventsArr = [...allEventsArray];
+        reversedEventsArr.reverse();
+        console.log(formEditBlock);
+        if (document.contains(formEditBlock)) {
+            formEditBlock.remove();
+            oneEvent.map((segment) => segment.style.display = `flex`);
+        }
+        oneEvent[counter].style.display = `none`;
+        renderBlocks(oneEventContainer[counter], createEditTripEvent(reversedEventsArr[counter]), `afterbegin`);
+        formEditBlock = document.querySelector(`.trip-events__item > form.event--edit`);
+        const arrowUpBtn = document.querySelector(`.event--edit > .event__header > .event__rollup-btn`);
+
+        function removeModal() {
+            this.parentNode.parentNode.remove();
+            oneEvent.map((segment) => segment.style.display = `flex`);
+        }
+        arrowUpBtn.addEventListener(`click`, removeModal);
+    }
+    arrowDownBtn.every((cell) => cell.addEventListener(`click`, renderSettingBlock));
+};
+saveButton.addEventListener(`click`, makingSaveEvent);
+
+
+
+
+const getTotalPrice = () => {
+    const totalSummBlock = document.querySelector(`.trip-info__cost-value`);
+    totalSummBlock.innerHTML = ``;
+    let totalPrice = 0;
+    for (let value of allEventsArray) {
+        totalPrice += value.price;
+        for (let offers of value.additionalOffers) {
+            if (offers.check === true) {
+                totalPrice += offers.price;
+            }
+        }
+
+    }
+    renderBlocks(totalSummBlock, totalPrice, `beforeend`);
 };
